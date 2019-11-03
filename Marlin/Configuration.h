1081e159aa4daca068d7992d9389f2ec7e28b125
sgh <sgh.nsk@gmail.com>
2019-11-03 17:39:16 +0700

    Автоподстройка pid под 240 градусов
    без обдува:
    16:55:47.825 : #define DEFAULT_Kp 11.19
    16:55:47.825 : #define DEFAULT_Ki 0.49
    16:55:47.826 : #define DEFAULT_Kd 63.25
    
    с обдувом:
    17:02:35.584 : #define DEFAULT_Kp 16.76
    17:02:35.584 : #define DEFAULT_Ki 0.96
    17:02:35.584 : #define DEFAULT_Kd 73.47

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -410,9 +410,9 @@
   //#define DEFAULT_Kd 440
 
   // Anycubic Kossel - run 'M106 S255' & 'M303 E0 C10 S200'
-  #define DEFAULT_Kp 14.24
-  #define DEFAULT_Ki 0.75
-  #define DEFAULT_Kd 67.36
+  #define DEFAULT_Kp 16.76
+  #define DEFAULT_Ki 0.96
+  #define DEFAULT_Kd 73.47
 
 #endif // PIDTEMP
 
